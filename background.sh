325c6a93ac52e5f8dea6a3ea1546d94db8fe0a5a
Jason DiPrinzio <jason@diprinzio>
2017-10-18 11:22:51 -0400

    background image loader

---
+++ background.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+xloadimage  -onroot  -xzoom 150 -yzoom 190 -fit ~/Pictures/26d7fc7a2b6c04ab74f06b301bbdf88f7bfd.jpg
--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+echo "copying background image to Pictures directory"
+cp 26d7fc7a2b6c04ab74f06b301bbdf88f7bfd.jpg ~/Pictures
+
 if [ -f install-files ]
 then
     files=`cat install-files`
