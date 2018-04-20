0bb8db34e175b9c19c14d1ab5b033d1c6161a9f9
Jason DiPrinzio <jason@diprinzio>
2018-04-20 16:06:41 -0400

    on the poll non-block refactors formats

--- watch_dir.cpp
+++ watch_dir.cpp
@@ -10,6 +10,7 @@
 #include <sys/time.h>
 #include <sys/types.h>
 
+#include <cstring>
 #include <string>
 #include <iostream>
 #include <sstream>
@@ -34,20 +35,19 @@ static void sig_handler(int sig)
 #if _POSIX_C_SOURCE >= 1 || _XOPEN_SOURCE || _POSIX_SOURCE
     sigset_t mask_set;
     sigset_t old_set;
-#endif  
+#endif
     signal(sig, sig_handler);
 #if _POSIX_C_SOURCE >= 1 || _XOPEN_SOURCE || _POSIX_SOURCE
     sigfillset(&mask_set);
     sigprocmask(SIG_SETMASK, &mask_set, &old_set);
 #endif
-
-    fprintf(stderr, "stopping....\n");
+    std::cerr << std::endl << "stopping..." << std::endl;
     RUN_FLAG = false;
 }
 
 static inline void timestamp()
 {
-    struct tm *timedata; 
+    struct tm *timedata;
     struct timeval tv;
     gettimeofday(&tv, NULL);
     time_t now = tv.tv_sec;
@@ -57,27 +57,18 @@ static inline void timestamp()
     fprintf(stdout, "[%s.%ld] ", datebuf, tv.tv_usec);
 }
 
-static inline void watch_event(const std::string& msg, const std::string& filename, const path_watcher& watcher, const struct inotify_event *event)
+static inline void watch_event(const std::string& msg, const std::string& filename, const path_watcher& , const struct inotify_event *event)
 {
     timestamp();
     fprintf(stdout, "[wd=%d,mask=x%08x,cookie=%u,length=%d] ", event->wd, event->mask, event->cookie, event->len);
-    std::cout << "path " << filename << " in watched path " << watcher.watchpath << " " << msg << std::endl;
+    std::cout << "path " << filename << " " << msg << std::endl;
 }
 
-static inline int print_file(const std::string& filename, const path_watcher& watcher)
+static inline int print_file(const std::string& filename)
 {
-    std::string fullpath;
-    if(filename == watcher.watchpath) {
-        fullpath = filename;
-    } else {
-        std::stringstream ss;
-        ss << watcher.watchpath << "/" << filename;
-        fullpath = ss.str();
-    }
-
-    std::ifstream file(fullpath, std::ios::binary);
+    std::ifstream file(filename, std::ios::binary);
     if(!file) {
-        std::cerr << "file " << filename << " cannot be read" << std::endl;
+        std::cerr << "file '" << filename << "' cannot be read" << std::endl;
         return 1;
     }
     std::cout << std::endl << "==========BEGIN FILE CONTENTS==========" << std::endl;
@@ -92,7 +83,7 @@ static inline int print_file(const std::string& filename, const path_watcher& wa
 
 static inline int format_timestamp(const time_t *timestamp, const struct timespec *usec, const int buflen, char *buf)
 {
-    struct tm *timedata; 
+    struct tm *timedata;
     timedata = localtime(timestamp);
     char datebuf[64];
     strftime(datebuf, 64, "%F %T", timedata);
@@ -100,37 +91,28 @@ static inline int format_timestamp(const time_t *timestamp, const struct timespe
     return 0;
 }
 
-static inline int stat_file(const std::string& filename, const path_watcher& watcher)
+static inline int stat_file(const std::string& filename)
 {
-    std::string fullpath;
-    if(filename == watcher.watchpath) {
-        fullpath = filename;
-    } else {
-        std::stringstream ss;
-        ss << watcher.watchpath << "/" << filename;
-        fullpath = ss.str();
-    }
-
     struct stat statbuf;
-    if(stat(fullpath.c_str(), &statbuf)) {
-        perror("cannot stat file");
+    if(stat(filename.c_str(), &statbuf)) {
+        std::cerr << "cannot stat file '" << filename << "' -  " << strerror(errno) << std::endl;
         return 1;
     }
 
     char *type = NULL;
-    if(S_ISREG(statbuf.st_mode) )         type = (char*)"regular file"; 
-    else if(S_ISDIR(statbuf.st_mode) )    type = (char*)"directory"; 
-    else if(S_ISCHR(statbuf.st_mode) )    type = (char*)"character device"; 
-    else if(S_ISBLK(statbuf.st_mode) )    type = (char*)"block device"; 
-    else if(S_ISFIFO(statbuf.st_mode))    type = (char*)"pipe"; 
-    else if(S_ISLNK(statbuf.st_mode) )    type = (char*)"symbolic link"; 
-    //else if(S_ISSOCK(statbuf.st_mode))    type = "socket"; 
+    if(S_ISREG(statbuf.st_mode) )         type = (char*)"regular file";
+    else if(S_ISDIR(statbuf.st_mode) )    type = (char*)"directory";
+    else if(S_ISCHR(statbuf.st_mode) )    type = (char*)"character device";
+    else if(S_ISBLK(statbuf.st_mode) )    type = (char*)"block device";
+    else if(S_ISFIFO(statbuf.st_mode))    type = (char*)"pipe";
+    else if(S_ISLNK(statbuf.st_mode) )    type = (char*)"symbolic link";
+    else if(S_ISSOCK(statbuf.st_mode))    type = (char*)"socket";
 
     int sbit    = (statbuf.st_mode >> 9)& 0x00000007;
     int omode   = (statbuf.st_mode >> 6)& 0x00000007;
     int gmode   = (statbuf.st_mode >> 3)& 0x00000007;
     int othmode = (statbuf.st_mode >> 0)& 0x00000007;
-    
+
     char atime[DF_BUFLEN], mtime[DF_BUFLEN], ctime[DF_BUFLEN];
     format_timestamp(&statbuf.st_atime, &statbuf.st_atim, DF_BUFLEN, atime);
     format_timestamp(&statbuf.st_mtime, &statbuf.st_mtim, DF_BUFLEN, mtime);
@@ -139,7 +121,7 @@ static inline int stat_file(const std::string& filename, const path_watcher& wat
     fprintf(stdout, "\n==========BEGIN METADATA==========\n");
     fprintf(stdout, "  File: '%s'\n  Size: %ld       Blocks: %ld       Inode: %ld       %s\n",
         filename.c_str(), statbuf.st_size, statbuf.st_blocks, statbuf.st_ino, type);
-    fprintf(stdout, "  Mode: %x%x%x%x     Uid: %u       Gid: %u\n", 
+    fprintf(stdout, "  Mode: %x%x%x%x     Uid: %u       Gid: %u\n",
         sbit,omode, gmode, othmode, statbuf.st_uid, statbuf.st_gid);
     fprintf(stdout, "Access: %s\nModify: %s\nChange: %s\n",
         atime, mtime, ctime);
@@ -156,7 +138,7 @@ void handle_event(const std::string& filename, const struct inotify_event *event
             break;
         case IN_ATTRIB :
             watch_event("metadata modified", filename, pw, event);
-            stat_file(filename, pw);
+            stat_file(filename);
             break;
         case IN_OPEN :
             watch_event("opened", filename, pw, event);
@@ -169,6 +151,7 @@ void handle_event(const std::string& filename, const struct inotify_event *event
             break;
         case IN_CREATE :
             watch_event("created", filename, pw, event);
+            break;
         case IN_DELETE :
             watch_event("removed", filename, pw, event);
             break;
@@ -177,17 +160,15 @@ void handle_event(const std::string& filename, const struct inotify_event *event
         case IN_MOVE :
             watch_event("moved", filename, pw, event);
             break;
-            /* We're not fast enough. */
         case IN_Q_OVERFLOW :
+            /* We're not fast enough. */
             fprintf(stderr, "event queue overflow");
             break;
-            /* Print the file contents since they changed. */
         case IN_MODIFY :
+            /* Print the file contents since they changed. */
             watch_event("modified", filename, pw, event);
             if(IS_DIR_EVENT(event->mask)  ) {
-                if(print_file(filename, pw)) {
-                    std::cerr << "cannot access ..." << filename << std::endl;
-                }
+                print_file(filename);
             }
             break;
     }
@@ -210,7 +191,7 @@ int main(int argc, const char **argv)
     watch_args wargs (watch_options_e::WATCH_OPT_RECURSE|watch_options_e::WATCH_OPT_REQUIRE_DIR,
             NOTIFY_FLAGS, NULL, paths, handle_event);
 #else
-    watch_args wargs (watch_options_e::WATCH_OPT_NONE, 
+    watch_args wargs (watch_options_e::WATCH_OPT_NONE,
             NOTIFY_FLAGS, NULL, paths, handle_event);
 #endif
     return watch(wargs, &RUN_FLAG);
--- watcher.cpp
+++ watcher.cpp
@@ -1,10 +1,11 @@
 #include <dirent.h>
 #include <errno.h>
-#include <stdio.h>
+#include <poll.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/stat.h>
 
+#include <cstring>
 #include <string>
 #include <sstream>
 #include <iostream>
@@ -24,7 +25,7 @@ static inline int is_dir(const std::string& path)
 {
     struct stat check_buf;
     if(stat(path.c_str(), &check_buf)){
-        perror("error stat");
+        std::cerr << "cannot check path: " << path << " - " << strerror(errno) << std::endl;
         return errno;
     }
     if(S_ISDIR(check_buf.st_mode)) {
@@ -38,7 +39,7 @@ static inline int check_is_dir(const std::string& path)
     if(is_dir(path)) {
         std::cerr << path << "is not a directory" << std::endl;
         return 1;
-    } 
+    }
     return 0;
 }
 
@@ -77,9 +78,9 @@ static int recurse_dir(watcher_map& watch_table, const std::string& dirname, ino
     dir = opendir(dirname.c_str());
     if(!dir){
         return 1;
-    }   
+    }
     struct dirent *entry = 0;
-    while( (entry = readdir(dir)) != NULL ) { 
+    while( (entry = readdir(dir)) != NULL ) {
         char *prefix = entry->d_name;
         if('.' == *prefix ) continue;
 
@@ -91,7 +92,7 @@ static int recurse_dir(watcher_map& watch_table, const std::string& dirname, ino
             /* TODO: Q these up to avoid notifications while we search for more directories. */
             add_watch(watch_table, fullname.str(), in_handle, wargs._watch_flags);
         }
-    }   
+    }
     closedir(dir);
     return 0;
 }
@@ -103,7 +104,7 @@ static void setup_watches(watcher_map& watch_table, const inotify_handle& in_han
             if(IS_DIR_REQUIRED(wargs._options) && check_is_dir(path)) {
                 continue;
             }
-            add_watch(watch_table, path, in_handle, wargs._watch_flags); 
+            add_watch(watch_table, path, in_handle, wargs._watch_flags);
         }
 
         if(IS_DIR_RECURSE(wargs._options)){
@@ -123,41 +124,28 @@ class auto_close
         const closeme& operator *() { return _closer; }
 };
 
-int watch(const watch_args& wargs, volatile bool *run_flag)
+int do_events(const watch_args& wargs, const inotify_handle in_handle, watcher_map& watch_table, volatile bool *run_flag)
 {
-    auto_close<inotify_handle> in_handle(inotify_init1(IN_NONBLOCK));
-
-    if(*in_handle < 0) {
-        perror("failed to initialize inotify");
-        return 1;
-    }
-
-    watcher_map watch_table;
-
-    setup_watches(watch_table, *in_handle, wargs);
-    
     char buf[BUF_LEN];
-    int retval = 0;
+	int retval = 0;
 
     while(!watch_table.empty() && *run_flag) {
         int len, i = 0;
-        len = read(*in_handle, buf, BUF_LEN);
+        len = read(in_handle, buf, BUF_LEN);
         if(len < 0) {
             if(errno == EINTR) {
-                break;
+                return EINTR;
             } else if(errno == EAGAIN) {
-                // pause, there will be data
-                usleep(5000);
-                continue;
+                break;
             } else {
                 perror("read error");
                 retval = errno;
                 *run_flag = false;
-                break;
+                return retval;
             }
         } else if(!len) {
             std::cerr << "no data from read" << std::endl;
-            continue;
+            break;
         }
 
         while(i<len) {
@@ -169,9 +157,11 @@ int watch(const watch_args& wargs, volatile bool *run_flag)
 
                 std::string filename;
                 if(event->len > 0) {
-                   filename = std::string(event->name);
+					std::stringstream ss;
+					ss << pw.watchpath << "/" << event->name;
+					filename = ss.str();
                 } else {
-                   filename = std::string(pw.watchpath);
+					filename = std::string(pw.watchpath);
                 }
 
                 wargs._callback(static_cast<const std::string>(filename), event, pw, nullptr);
@@ -179,17 +169,17 @@ int watch(const watch_args& wargs, volatile bool *run_flag)
                 switch(event->mask & 0x00FFFFFF) {
                     case IN_CREATE :
                         if(is_dir(filename) == 0)
-                            add_watch(watch_table, static_cast<std::string>(filename), *in_handle, wargs._watch_flags);
+                            add_watch(watch_table, static_cast<std::string>(filename), in_handle, wargs._watch_flags);
                         break;
-                        /* Cases where are watchers become invalid. */
+                    /* Cases where are watchers become invalid. */
                     case IN_DELETE_SELF :
-                        remove_watch(watch_table, "watched path was removed", event, pw, *in_handle);
+                        remove_watch(watch_table, "watched path was removed", event, pw, in_handle);
                         break;
                     case IN_MOVE_SELF :
-                        remove_watch(watch_table, "watched path was moved", event, pw, *in_handle);
+                        remove_watch(watch_table, "watched path was moved", event, pw, in_handle);
                         break;
                     case IN_UNMOUNT :
-                        remove_watch(watch_table, "NFS partition unmounted", event, pw, *in_handle);
+                        remove_watch(watch_table, "NFS partition unmounted", event, pw, in_handle);
                         break;
                 }
             } catch(std::out_of_range& e) {
@@ -199,7 +189,45 @@ int watch(const watch_args& wargs, volatile bool *run_flag)
         }
     }
 
-    std::cerr << "quitting: no active watch descriptors" << std::endl;
+	return 0;
+}
+
+int watch(const watch_args& wargs, volatile bool *run_flag)
+{
+    auto_close<inotify_handle> in_handle(inotify_init1(IN_NONBLOCK));
+
+    if(*in_handle < 0) {
+        perror("failed to initialize inotify");
+        return errno;
+    }
+
+    watcher_map watch_table;
+
+    setup_watches(watch_table, *in_handle, wargs);
+
+    int retval = 0;
+	nfds_t nfds = 1;
+	struct pollfd fds[2];
+
+	fds[0].fd = *in_handle;
+	fds[0].events = POLLIN;
+
+	while (*run_flag) {
+		int poll_num = poll(fds, nfds, -1);
+		if (poll_num == -1) {
+			if (errno == EINTR)
+				return errno;
+			retval = errno;
+			break;
+		}
+		if (poll_num > 0 && (fds[0].revents & POLLIN)) {
+			retval = do_events(wargs, *in_handle, watch_table, run_flag);		
+			if(watch_table.empty()) {
+				std::cerr << "quitting: no active watch descriptors" << std::endl;
+				break;
+			}
+		}
+	}
 
     return retval;
 }
