07603e0c57a12a8d6f1df9b8b606c1b69a3a1a45
Jason DiPrinzio <jason@diprinzio>
2019-04-09 10:52:29 -0400

    new dev setup

---
+++ dev-setup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+mkdir ~/Projects
+
+git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+$(cd ~/.oh-my-zsh/custom/themes && git clone https://github.com/bhilburn/powerlevel9k.git)
+scp -P8001 jason@jason.diprinz.io:powertheme.patch ~/.oh-my-zsh/custom/themes/powerlevel9k/powertheme.patch
+$(cd ~/.oh-my-zsh/custom/themes/powerlevel9k && git apply powertheme.patch)
+git clone https://github.com/powerline/fonts.git ~/Projects/fonts
+$(cd ~/Projects/fonts && ./install.sh)
+scp -P8001 jason@jason.diprinz.io:zshrc ~/.zshrc
+git clone ssh://jason@jason.diprinz.io:8001/volume3/git/repos/vim-config.git ~/.oh-my-vim
+scp -P8001 jason@jason.diprinz.io:vimrc ~/.vimrc
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+curl -L "https://github.com/Kitware/CMake/releases/download/v3.14.1/cmake-3.14.1.tar.gz" > ~/Projects/cmake-3.14.1.tar.gz
+$(cd ~/Projects; tar zxvf cmake-3.14.1.tar.gz)
+$(cd ~/Projects/cmake-3.14.1/ && ./bootstrap; make && make install)
+$(cd ~/.vim/bundle/YouCompleteMe; ./install.py --clang-completer --java-completer --go-completer)
+
