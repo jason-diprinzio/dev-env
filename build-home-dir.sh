ace91ef00ce873e3bdce094af3c6809b4ff7d28e
Jason Diprinzio <jason@DiPrinzio>
2023-09-20 09:43:28 -0400

    mac returns

--- build-home-dir.sh
+++ build-home-dir.sh
@@ -6,5 +6,10 @@ cp vimrc ~/.vimrc
 cp tmux.conf ~/.tmux.conf
 cp ycm_extra_conf.py ~/.ycm_extra_conf.py
 cp gitignore ~/.gitignore
-cp xmonad.hs ~/.xmonad/xmonad.hs
-cp i3-config ~/.config/i3/config
+
+if [ "Linux" == $(uname) ]; then
+	mkdir ~/.xmonad
+	cp xmonad.hs ~/.xmonad/xmonad.hs
+	mkdir ~/.config/i3
+	cp i3-config ~/.config/i3/config
+fi
--- dev-setup.sh
+++ dev-setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -v
-base_dir=~/dev-env
-install_dir=/home/jason/projects
+base_dir=~/projects/dev-env
+install_dir=~/projects
 
 mkdir -p "${install_dir}"
 
@@ -14,12 +14,18 @@ $(cd ~/.oh-my-zsh/custom/themes/powerlevel9k && git apply "${base_dir}/powerthem
 git clone https://github.com/powerline/fonts.git "${install_dir}"/fonts
 $(cd "${install_dir}"/fonts && bash -c ./install.sh)
 
-$(cd "${install_dir}" && git clone https://github.com/vim/vim.git)
-$(cd "${install_dir}"/vim && bash -c ./configure --prefix=/usr/local --with-compiledby=jasondiprinzio --with-features=huge --enable-multibyte --enable-channel --enable-terminal --enable-python3interp=dynamic --enable-perlinterp --enable-xim)
-$(cd "${install_dir}"; make && sudo make install)
+python_version=$(python3 --version | cut -d " " -f 2 | cut -d "." -f1-2)
+if [ "Darwin" == $(uname) ]; then
+    export CFLAGS="-isystem /System/Volumes/Data/Library/Developer/CommandLineTools/Library/Frameworks/Python3.framework/Headers"
+    extra_params=--with-python3-config-dir=/System/Volumes/Data/Library/Developer/CommandLineTools/Library/Frameworks/Python3.framework/Versions/Current/lib/python${python_version}/config-${python_version}-darwin
+fi
 
-git clone https://github.com/jason-diprinzio/vim-config.git ~/.oh-my-vim
+$(cd "${install_dir}" && git clone https://github.com/vim/vim.git)
+$(cd "${install_dir}"/vim && bash -c ./configure --prefix=/usr/local --with-compiledby=jasondiprinzio --with-features=huge --enable-multibyte --enable-channel --enable-terminal --enable-python3interp=dynamic --enable-perlinterp --enable-xim ${extra_params})
+$(cd "${install_dir}"; make -j && sudo make install)
+
+git clone https://github.com/jason-diprinzio/oh-my-vim.git ~/.oh-my-vim
 cp "${base_dir}"/vimrc ~/.vimrc
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
-#$(cd ~/.vim/bundle/YouCompleteMe; ./install.py --clang-completer --java-completer --go-completer --rust-completer --ts-completer)
+$(cd ~/.vim/bundle/YouCompleteMe; ./install.py --clang-completer --java-completer --go-completer --rust-completer --ts-completer)
